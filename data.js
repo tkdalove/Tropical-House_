c65bf2b95beb362dec27efcfd14c4daa62e4fe76
tkdalove <sumate4454@gmail.com>
2025-05-09 19:53:46 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,279 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-front-view",
+      "name": "Front view",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.14834419845646707,
+          "pitch": 0.09487192674564326,
+          "rotation": 0,
+          "target": "1-living-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-living-room",
+      "name": "Living room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5497526495037874,
+          "pitch": 0.25674081246094715,
+          "rotation": 0,
+          "target": "2-bed-room-1"
+        },
+        {
+          "yaw": 0.8268736865920054,
+          "pitch": 0.20188700445023677,
+          "rotation": 0,
+          "target": "4-bed-room-2"
+        },
+        {
+          "yaw": 1.1403553834654474,
+          "pitch": 0.20657121116789234,
+          "rotation": 0.7853981633974483,
+          "target": "5-bath-room-2"
+        },
+        {
+          "yaw": 2.1738603636862663,
+          "pitch": 0.21627883331117737,
+          "rotation": 1.5707963267948966,
+          "target": "6-multipurpose-room"
+        },
+        {
+          "yaw": -1.9513336052814019,
+          "pitch": 0.1875072305905583,
+          "rotation": 0,
+          "target": "0-front-view"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-bed-room-1",
+      "name": "Bed room 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7544443604142117,
+          "pitch": 0.29998403063813406,
+          "rotation": 4.71238898038469,
+          "target": "3-bath-room-1"
+        },
+        {
+          "yaw": 1.2358289234904056,
+          "pitch": 0.3207698733190263,
+          "rotation": 1.5707963267948966,
+          "target": "1-living-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-bath-room-1",
+      "name": "Bath room 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.710398938149221,
+          "pitch": 0.33719273177207043,
+          "rotation": 0,
+          "target": "2-bed-room-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-bed-room-2",
+      "name": "Bed room 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.836315348616762,
+          "pitch": 0.2713790533002367,
+          "rotation": 0,
+          "target": "1-living-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-bath-room-2",
+      "name": "Bath room 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.5497183253990023,
+          "pitch": 0.2085434699825175,
+          "rotation": 0,
+          "target": "1-living-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-multipurpose-room",
+      "name": "Multipurpose Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.276343672496628,
+          "pitch": 0.2877667944756457,
+          "rotation": 0,
+          "target": "1-living-room"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Tropical House",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
